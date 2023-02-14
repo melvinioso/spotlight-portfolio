8112e211051fef535ef3c399d12bedbca796dbf7
Matt McGavern <matt.mcgavern@gmail.com>
2023-02-14 00:45:53 -0600

    spelling fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -248,7 +248,7 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Full Stack Developer, husband, dog dad, and amatuer disc golfer.
+            Full Stack Developer, husband, dog dad, and amateur disc golfer.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Matt, a full stack web developer based out of Austin, TX.
